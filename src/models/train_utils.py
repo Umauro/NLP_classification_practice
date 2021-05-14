9f7f9e5adceecf2254774bb8e92ddb75a14d933e
Felipe Vega <felipe.vega.14@sansano.usm.cl>
2021-05-14 19:02:52 -0400

    refactor argument parsing and validation into TrainConfig Class
    call validate_config inside TrainConfig __init__

---
+++ src/models/train_utils.py
@@ -0,0 +1,79 @@
+import warnings
+
+def setting_argument_warning(bool_arg: str, arg_name: str, arg_default: str):
+    warnings.warn(
+        '{} is disabled but {} was not specified. Setting n_hidden_units to {}'
+            .format(bool_arg, arg_name, arg_default)
+    )
+
+class TrainConfig:
+    """
+        Class with training configuration params
+    """
+    def __init__(self, parser):
+        args = parser.parse_args()
+        self.parser = parser
+        self.csv_path = args.csv_path
+        self.val_set_size = args.val_set_size
+        self.max_epochs = args.max_epochs
+        self.batch_size = args.batch_size
+        self.initial_lr = args.initial_lr
+        self.log_path = args.log_path
+        self.log_name = args.log_name
+        self.checkpoint = args.checkpoint
+        self.checkpoint_path = args.checkpoint_path
+        self.load_config = args.load_config
+        self.config_path = args.config_path
+        self.n_hidden_units = args.n_hidden_units
+        self.hidden_units = args.hidden_units
+        self.use_dropout = args.use_dropout
+        self.dropout_prob = args.dropout_prob
+        self.bert_pooled_output = args.bert_pooled_output
+
+        self.validate_config()
+
+    def validate_config(self):
+        """
+            Validate some config params values
+        """
+        if self.val_set_size <= 0:
+            self.parser.error('val_set_size must be greater than 0')
+
+        if self.max_epochs < 1:
+            warnings.warn('max_epochs must be greater than 0. Setting max_epochs to 1.')
+            self.max_epochs = 1
+
+        if self.batch_size < 1:
+            warnings.warn('batch_size must be greater than 0. Setting batch_size to 32.')
+            self.batch_size = 32
+
+        if self.initial_lr < 0:
+            self.parser.error('initial lr must be positive.')
+
+        if self.checkpoint and self.checkpoint_path is None:
+            self.parser.error('--checkpoint is activated but checkpoint_path was not specified.')
+
+        if self.load_config and self.config_path is None:
+            self.parser.error('--load_config is activated but config_path was not specified.')
+
+        if not self.load_config:
+            if self.n_hidden_units is None:
+                setting_argument_warning('load_config', 'n_hidden_units', '1')
+                self.n_hidden_units = 1
+            
+            if self.hidden_units is None:
+                setting_argument_warning('load_config', 'hidden_units', '[512]')
+                self.hidden_units = [512]
+            
+            if self.use_dropout is None:
+                setting_argument_warning('load_config', 'use_dropout', 'False')
+                self.use_dropout = False
+            
+            if self.dropout_prob is None:
+                setting_argument_warning('load_config', 'dropout_prob', '0')
+                self.dropout_prob = 0
+            
+            if self.bert_pooled_output is None:
+                setting_argument_warning('load_config', 'bert_pooled_output', '768')
+                self.bert_pooled_output = 768
+    
--- train.py
+++ train.py
@@ -1,19 +1,14 @@
 import argparse
-import warnings
 import pandas as pd
 
+from src.models.train_utils import TrainConfig
 from sklearn.model_selection import train_test_split 
 from src.data.dfs_schemas import ProcessedDataframeSchema
 
 
 MAX_SEQ_LEN = 128
 
-def setting_argument_warning(bool_arg: str, arg_name: str, arg_default: str):
-    warnings.warn(
-        '{} is disabled but {} was not specified. Setting n_hidden_units to {}'
-            .format(bool_arg, arg_name, arg_default)
-    )
-    
+  
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Arguments for training')
     parser.add_argument('--csv_path', type = str, required = True)
@@ -33,68 +28,11 @@ if __name__ == '__main__':
     parser.add_argument('--dropout_prob', type = float, required = False)
     parser.add_argument('--bert_pooled_output', type = int, required = False)
 
-    args = parser.parse_args()
-    csv_path = args.csv_path
-    val_set_size = args.val_set_size
-    max_epochs = args.max_epochs
-    batch_size = args.batch_size
-    initial_lr = args.initial_lr
-    log_path = args.log_path
-    log_name = args.log_name
-    checkpoint = args.checkpoint
-    checkpoint_path = args.checkpoint_path
-    load_config = args.load_config
-    config_path = args.config_path
-    n_hidden_units = args.n_hidden_units
-    hidden_units = args.hidden_units
-    use_dropout = args.use_dropout
-    dropout_prob = args.dropout_prob
-    bert_pooled_output = args.bert_pooled_output
-
-    if val_set_size <= 0:
-        parser.error('val_set_size must be greater than 0')
-
-    if max_epochs < 1:
-        warnings.warn('max_epochs must be greater than 0. Setting max_epochs to 1.')
-        max_epochs = 1
-
-    if batch_size < 1:
-        warnings.warn('batch_size must be greater than 0. Setting batch_size to 32.')
-        batch_size = 32
-
-    if initial_lr < 0:
-        parser.error(message = 'initial lr must be positive.')
-
-    if checkpoint and checkpoint_path is None:
-        parser.error(message = '--checkpoint is activated but checkpoint_path was not specified.')
-
-    if load_config and config_path is None:
-        parser.error(message = '--load_config is activated but config_path was not specified.')
-
-    if not load_config:
-        if n_hidden_units is None:
-            setting_argument_warning('load_config', 'n_hidden_units', '1')
-            n_hidden_units = 1
-        
-        if hidden_units is None:
-            setting_argument_warning('load_config', 'hidden_units', '[512]')
-            hidden_units = [512]
-        
-        if use_dropout is None:
-            setting_argument_warning('load_config', 'use_dropout', 'False')
-            use_dropout = False
-        
-        if dropout_prob is None:
-            setting_argument_warning('load_config', 'dropout_prob', '0')
-            dropout_prob = 0
-        
-        if bert_pooled_output is None:
-            setting_argument_warning('load_config', 'bert_pooled_output', '768')
-            bert_pooled_output = 768
+    train_config = TrainConfig(parser)
 
     # LOAD DATA
     processed_schema = ProcessedDataframeSchema()
-    df = pd.read_csv(csv_path, sep = ',')
+    df = pd.read_csv(train_config.csv_path, sep = ',')
 
     # Validate Correct DataFrame 
     validation_warnings = processed_schema.check_schema(df)
@@ -108,7 +46,7 @@ if __name__ == '__main__':
     # Validation split
     df_train, df_val = train_test_split(
         df,
-        test_size = val_set_size, 
+        test_size = train_config.val_set_size, 
         stratify = df.original_label.values,
         random_state = 42 #always use the same validation set
     ) 
